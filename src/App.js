52bfd4ff7e8744ba2c22b6e279e4adcdb72e8132
PARKDL <ehddlf2395@gmail.com>
2019-08-08 17:10:36 +0900

    styles

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
-                genre={movie.genres}
+                genres={movie.genres}
               />
             ))}
           </div>
